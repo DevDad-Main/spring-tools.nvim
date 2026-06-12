43306de576387f00ec483882762c0c23f3b0de05
Oliver Metz <softwaredevdad@gmail.com>
2026-06-12 17:19:52 +0200

    fix: remove nvim_buf_set_name to avoid buffer name collision

--- lua/spring-tools/ui/init.lua
+++ lua/spring-tools/ui/init.lua
@@ -13,7 +13,6 @@ function M.create_float_win(opts)
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_option(buf, "buftype", "nofile")
   vim.api.nvim_buf_set_option(buf, "modifiable", true)
-  vim.api.nvim_buf_set_name(buf, opts.title or "spring-tools")
 
   local win = vim.api.nvim_open_win(buf, true, {
     relative = "editor",
